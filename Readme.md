f9d32582b8298788d3b06d9e36b63961169519ed
ErikPereira <erik_bezerra_silva@hotmail.com>
2020-09-18 09:57:47 -0300

    18/09: modificacoes caixa

--- Readme.md
+++ Readme.md
@@ -3,3 +3,8 @@
 Criação de API para o projeto Integrado
 
 esse cara ajudou muito: https://www.youtube.com/watch?v=LCGqOjO7Zc8
+
+# Executar código
+
+entre na pasta *functions* e execute o comando
+`firebase deploy`
--- functions/services/user.Caixa.js
+++ functions/services/user.Caixa.js
@@ -2,38 +2,88 @@ const admin = require('firebase-admin');
 
 module.exports = {
 
-    novaCaixa: function(request, response){
-        const novaCaixa = {
-            nomeCaixa: request.body.nomeCaixa,
-            id: request.body.idCaixa
-        };
-        const id = request.body.idUsuario;
-        const dbCaixa = admin.firestore().collection("Usuario").doc(id);
-        let data = {};
+    novaCaixa: async function(request, response){
+        const idCaixa =  request.body.idCaixa;
+        const idUsuario = request.body.idUsuario;
+        const dbUsuario = admin.firestore().collection("Usuario").doc(idUsuario);
+        const dbCaixa = admin.firestore().collection("Caixa").doc(idCaixa);
+        const mudarCliente = request.body.mudarCliente;
+        let caixaCadastrada = {};
 
-        dbCaixa.get()
-        .then(function(doc){
-            data = {
-                alarmes: doc.data().alarmes,
-                caixas: doc.data().caixas,
-                login: doc.data().login
-            }
-
-            data.caixas.push(novaCaixa);
-            dbCaixa.update(data)
-            .then(function(){
-                response.status(200).json({
-                    response: true,
-                    msg: "Caixa Adicionada com sucesso!",
+        await dbCaixa.get()
+            .then(function(doc){
+                caixaCadastrada = {
+                    emailCliente: doc.data().emailCliente,
+                    celularCliente: doc.data().celularCliente,
+                    nome: doc.data().nome,
+                    id: doc.data().id
+                }; 
+            })
+            .catch(function(err){
+                response.status(404).json({
+                    response: false,
+                    msg: "caixa "+ idCaixa +" Não encontrada: "
                 });
             })
+            
+        if(caixaCadastrada.emailCliente !== "null" && !mudarCliente){
+            response.status(304).json({
+                response: false,
+                msg: "caixa "+ idCaixa + " já foi cadastrada"
+            });
+            return;
+        }
+
+        let usuarioCadastrado = {};
+        
+        await dbUsuario.get()
+            .then(function(doc){
+                usuarioCadastrado = {
+                    alarmes: doc.data().alarmes,
+                    caixas: doc.data().caixas,
+                    login: doc.data().login
+                }
+            })
+            .catch(function(err){
+                response.status(404).json({
+                    response: false,
+                    msg: "Usuario "+ idUsuario +" Não encontrada: "
+                });
+            })
+        
+        usuarioCadastrado.caixas.push({
+            id: idCaixa,
+            nome: request.body.nomeCaixa
+        });
+
+        caixaCadastrada = {
+            emailCliente: usuarioCadastrado.login.email,
+            celularCliente: usuarioCadastrado.login.celular,
+            nome: request.body.nomeCaixa,
+            id: idCaixa
+        }
+
+        await dbCaixa.update(caixaCadastrada)
             .catch(function(err){
                 response.status(304).json({
                     response: false,
-                    msg: "Caixa não adicionado!" + err,
+                    msg: "erro ao salvar informacoes da Caixa!" + err,
                 });
-            })  
-        })
+            })
+
+        await dbUsuario.update(usuarioCadastrado)
+            .catch(function(err){
+                response.status(304).json({
+                    response: false,
+                    msg: "erro ao salvar informacoes do Usuario!" + err,
+                });
+            }) 
+        
+        response.status(200).json({
+            response: true,
+            msg: "Caixa adicionada com sucesso!",
+        });
+        
     },
 
     atualizaCaixa: function(request, response){
