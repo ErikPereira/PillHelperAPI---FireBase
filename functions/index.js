92d227e0cc965d1f59bf2d25778a241d3c4bb524
ErikPereira <erik_bezerra_silva@hotmail.com>
2021-04-27 18:57:44 -0300

    IMP-27/04: start

--- functions/index.js
+++ functions/index.js
@@ -8,45 +8,11 @@ const userCaixa = require('./services/user.Caixa');
 const userAlarme = require('./services/user.Alarme');
 //const db = admin.firestore().collection("Usuario");
 
-/**Usuario: get Usuarios 
- * @return <Array> Usuarios - lista com todos os usuarios cadastrados
- */ 
-
 app.post("/Usuario/cadastrado", userUsuarios.getUsuarios);
+
 app.get("/Usuario/todos", userUsuarios.todosUsuarios);
 
-/**Usuario: criar novo Usuario
- * @param request.body - json exemplo:
- * {
- *     "alarmes": 
- *      [
- *          {
- *              "hora": "03:55",
- *              "nomeRemedio": "Vida"
- *          }
- *      ],
- *      "caixas": 
- *      [
- *          {
- *              "id": 7
- *          }
- *      ],
- *      "login": {
- *          "celular": 11985611024,
- *          "senha": "3145",
- *          "email": "email_muito_elevado_aleatorio@hotmail.com"
- *      }
- *  }
- * @return <boolean> response
- * @return <String> msg - Informações
- */
 app.post("/Usuario", userUsuarios.postUsuario);
-
-/** Usuario/login: verifica se o usuario e senha já são cadastrados
- * @param request.body - json exemplo: {email: "", celular: "", senha: ""}
- * @return <boolean> response
- * @return <String> msg - Informações
- */
 
 app.post("/Usuario/login", userLogin.postLogin);
 
